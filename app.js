45e9b380349c0dd0b6339293d679d63a43428bb5
wanghao <529036324@qq.com>
2018-07-11 14:47:22 +0800

    2048游戏界面初始化

--- app.js
+++ app.js
@@ -6,32 +6,26 @@ App({
     logs.unshift(Date.now())
     wx.setStorageSync('logs', logs)
 
-    // 登录
-    wx.login({
-      success: res => {
-        // 发送 res.code 到后台换取 openId, sessionKey, unionId
-      }
-    })
-    // 获取用户信息
-    wx.getSetting({
-      success: res => {
-        if (res.authSetting['scope.userInfo']) {
-          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
+    //获取用户信息
+    this.getUserInfo()
+  },
+  getUserInfo: function(usinfo){
+    var that = this
+    if(this.globalData.userInfo){
+      typeof usinfo == 'function' && usinfo(this.globalData.userInfo)
+    }else{
+      // 登录
+      wx.login({
+        success: res => {
           wx.getUserInfo({
-            success: res => {
-              // 可以将 res 发送给后台解码出 unionId
-              this.globalData.userInfo = res.userInfo
-
-              // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-              // 所以此处加入 callback 以防止这种情况
-              if (this.userInfoReadyCallback) {
-                this.userInfoReadyCallback(res)
-              }
+            success: function(res){
+              that.globalData.userInfo = res.userInfo
+              typeof usinfo == 'function' && usinfo(that.globalData.userInfo)
             }
           })
         }
-      }
-    })
+      })
+    }
   },
   globalData: {
     userInfo: null
--- pages/index/index.js
+++ pages/index/index.js
@@ -1,15 +1,69 @@
 //index.js
+
 //获取应用实例
 const app = getApp()
 
 Page({
   data: {
+    mydata: new Array(16),
+    slidetime: 20,
+    direction: 0,
+    startPos: {},
+    gameover: {
+      status: true,
+      message: 'game over'
+    },
+    userinfo: {}
   },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-    })
+  onLoad: function(){
+      //获取用户信息
+      this.data.userinfo = app.globalData.userInfo
+      //初始化数组
+      this.data.mydata.fill(0)
+      //随机生成2个数字
+      this.randNum()
+      this.randNum()
   },
-  onLoad: function () {
+  randNum: function(){
+      var arr = []
+      this.data.mydata.map(function(item,i){
+        if(item == 0){
+          arr.push(i)
+        }
+      })
+      var num = Math.random()>0.5?4:2
+      while (true) {
+        var index = arr[Math.floor(Math.random() * arr.length)]
+        console.log(this.data.mydata[index])
+        if (this.data.mydata[index] != 0) {
+          this.randNum();
+        } else {
+          break;
+        }
+      }
+      this.changeData(index, num)
+  },
+  changeData: function(index,num){
+    var changedData = {}
+    changedData['mydata['+ index + ']'] = num
+    //将数据从逻辑层发送到视图层（异步），同时改变对应的 this.data 的值（同步）
+    this.setData(changedData) 
+  },
+  //开始滑动
+  start: function(e){
+    //touches数组对象获得屏幕上所有的touch，取第一个touch
+    var touch = e.touches(0)
+    //取第一个touch的坐标值
+    this.data.startPos = {x:touch.pageX,y:touch.pageY,time:new Date()}
+    this.data.direction = 0;
+  },
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+    return {
+      title: '咪咪2048新玩法，赶紧来试试吧',
+      path: '/page/index'
+    }
   }
 })
